bd780d4e06b801d67cf1955004e7e7a3beff663f
Tony R Quilkey <trq@thorpesystems.com>
2014-01-28 20:24:04 +1100

    Fix my dotfiles.

--- modules/people/manifests/trq/dotfiles.pp
+++ modules/people/manifests/trq/dotfiles.pp
@@ -10,52 +10,8 @@ class people::trq::dotfiles {
     require => Package["vim"]
   }
 
-  file { "/Users/${::boxen_user}/.vimrc":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/vimrc",
-    require => Repository["dotfiles_repo"],
-    notify => Exec['install_vim_plugins']
-  }
-
-  file { "/Users/${::boxen_user}/.vim":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/vim",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/.bashrc":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/mac-bashrc",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/.bash_profile":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/bash_profile",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/.gitconfig":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/mbp.gitconfig",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/.inputrc":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/inputrc",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/.tmux.conf":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/tmux.conf",
-    require => Repository["dotfiles_repo"]
-  }
-
-  file { "/Users/${::boxen_user}/Library/Preferences/com.googlecode.iterm2.plist":
-    ensure => 'link',
-    target  => "/Users/${::boxen_user}/etc/dotfiles/mbp.com.googlecode.iterm2.plist",
+  exec { "install_dotfiles":
+    command => "/Users/${::boxen_user}/etc/dotfiles/bin/link-dotfiles",
     require => Repository["dotfiles_repo"]
   }
 }
